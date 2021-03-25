3b270b1945c6ba37b15ba4370cb91e3e623ab1cd
Sai Krishna Sahu <saikrishnasahu2012@gmail.com>
2021-03-25 13:08:34 +0530

    initial commit

---
+++ src/main/java/com/example/apachebeam/wordcount/mapper/ResultMapper.java
@@ -0,0 +1,20 @@
+package com.example.apachebeam.wordcount.mapper;
+
+import com.example.apachebeam.wordcount.model.Account;
+import com.example.apachebeam.wordcount.model.LoanAccount;
+import com.example.apachebeam.wordcount.model.Result;
+
+public class ResultMapper {
+
+    public static void buildResult(Result result, Account account) {
+        result.setAccountNo(account.getAccountNo());
+        result.setFirstName(account.getFirstName());
+        result.setBalance(account.getBalance());
+    }
+
+    public static void buildResult(Result result, LoanAccount loanAccount) {
+        result.setLoanAccountNo(loanAccount.getLoanAccountNo());
+        result.setLoanOutstanding(loanAccount.getLoanOutstanding());
+        result.setLoanAmount(loanAccount.getLoanAmount());
+    }
+}
